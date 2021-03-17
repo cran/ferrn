fc2ffea2018a9da309cd6029ccce7a33096fad4b
H. Sherry Zhang <huize.zhang@monash.edu>
2021-03-17 12:20:16 +0000

    version 0.0.1

---
+++ R/utils-pipe.R
@@ -0,0 +1,12 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @return this is a magrittr pipe!
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
